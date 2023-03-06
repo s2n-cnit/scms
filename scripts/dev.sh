5759d633211e83479b26cb6513d746e5b9073007
Alex Carrega <contact@alexcarrega.com>
2023-03-06 16:14:46 +0100

    Major changes

--- scripts/dev.sh
+++ scripts/dev.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Copyright (c) 2022-2029 TNT-Lab (https://github.com/s2n-cnit/scms)
+# Copyright (c) 2022-2029 S2N National Lab @ CNIT (https://github.com/s2n-cnit/scms)
 # author: Alex Carrega <alessandro.carrega@unige.it>
 
 python3 src/dev.py
